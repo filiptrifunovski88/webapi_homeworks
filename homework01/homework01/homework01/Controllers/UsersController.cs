45f3276c421eefc46d9387aa265563f12a8fb84b
Filip Trifunovski <filiptrifunovski88@gmail.com>
2025-08-08 03:18:10 +0200

    homework01

---
+++ homework01/homework01/homework01/Controllers/UsersController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace homework01.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAllUsers()
+        {
+            return Ok(StaticDb.Usernames);
+        }
+
+        [HttpGet("oneuser")]
+        public IActionResult GetOneUser()
+        {
+            return Ok(StaticDb.Usernames[1]);
+        }
+    }
+}
---
+++ homework01/homework01/homework01/StaticDb.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace homework01
+{
+    public static class StaticDb
+    {
+        public static List<string> Usernames = new List<string>()
+        {
+                "filip123",
+                "trifunovski",
+                "filiptrifunovski123"
+        };
+    }
+}
